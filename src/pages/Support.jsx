bc99bbe1fe688f859c863546fd2ca938f7836124
victorOkutachi <victorokutachi@gmail.com>
2024-06-06 12:00:08 +0100

    submission last github

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -4,7 +4,21 @@ import React from 'react'
 const Support = () => {
   return (
 
-    <></>
+    <>
+     <div className='flex flex-col w-full items-center justify-center'>
+                <h2 className='text-[20px] md:text-[30px] text-[#eba95d] font-Dancing mb-3 text-bold'>
+                    Donate Directly
+                </h2>
+
+                <p className='max-w-[450px] text-[16px] md:text-[22px] font-Kanit md:max-w-[750px] px-6'>
+                Your direct contributions, no matter the size, make a direct impact on IamElswag's ability to create and share music. 
+                Every dollar contributes to the production of new tracks, high-quality recordings, and the overall enhancement of the musical 
+                experience.
+                </p>
+
+                <button className='text-white bg-[#eba95d] font-bold text-[14px] md:text-[18px] rounded-full hover:text-white border-[2px] hover:bg-[#eba95d] border-white hover:border-white px-10 py-3 md:w-[200px] md:py-3 md:px-8 w-[120px] flex items-center justify-center my-5 mx-auto md:mx-0'>Donate</button>
+            </div>
+    </>
 
   )
 }
